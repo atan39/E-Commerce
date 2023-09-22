1972aeae63a182f4698d32214919569ea8704a12
Ashley Tan <ashleytan39@gmail.com>
2023-09-21 22:53:50 -0500

    updated category routes

--- models/index.js
+++ models/index.js
@@ -5,14 +5,30 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
-Product.belongsTo(Catergory, {
+Product.belongsTo(Category, {
+  foreignKey: 'category_id',
+});
 
-})
 // Categories have many Products
-Catergory.hasMany(Products)
-// Products belongToMany Tags (through ProductTag)
+Category.hasMany(Product, {
+  foreignKey: 'category_id',
+  onDelete: 'CASCADE',
+});
 
+// Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, {
+  through: {
+    model: ProductTag,
+    unique: false
+  }
+});
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  through: {
+    model: ProductTag,
+    unique: false
+  }
+});
 
 module.exports = {
   Product,
